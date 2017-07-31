042aad7f3e8fd91916b3fdf0e6e985179ce717e4
tamerk19-meet <tamerk19@meet.mit.edu>
2017-07-31 17:25:08 +0300

    Stopping here

---
+++ keys.py
@@ -0,0 +1,33 @@
+def up():
+    global direction
+    direction=UP
+    print('you pressed up')
+def left():
+    global direction
+    direction=LEFT
+    print('you pressed left')
+def down():
+    global direction
+    direction=DOWN
+    print('you pressed down')
+def right():
+    global direction
+    direction=RIGHT
+    print('you pressed right')    
+###################################################
+UP_ARROW='Up'
+UP_ARROW='Left'
+UP_ARROW='Down'
+UP_ARROW='Right'
+SPACEBAR='space'
+
+UP='0'
+LEFT='1'
+DOWN='2'
+RIGHT='3'
+direction=UP
+def up():
+    direction=UP
+    UP_ARROW
+    print('up arrow')
+
