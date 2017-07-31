1d0c9f59ee404cc437beca114f742c10e95ea7f9
tamerk19-meet <tamerk19@meet.mit.edu>
2017-07-31 18:36:10 +0300

    Stopping here

--- keys.py
+++ keys.py
@@ -2,32 +2,63 @@ def up():
     global direction
     direction=UP
     print('you pressed up')
+    turtle.pos()
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y+10)
+    print(turtle.pos())
 def left():
     global direction
     direction=LEFT
     print('you pressed left')
+    turtle.pos()
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x-10,y)
+    print(turtle.pos())
 def down():
     global direction
     direction=DOWN
     print('you pressed down')
+    turtle.pos()
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y-10)
+    print(turtle.pos())
 def right():
     global direction
     direction=RIGHT
-    print('you pressed right')    
+    print('you pressed right')
+    turtle.pos()
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x+10,y)
+    print(turtle.pos())
 ###################################################
+import turtle
+turtle.shape('turtle')
 UP_ARROW='Up'
-UP_ARROW='Left'
-UP_ARROW='Down'
-UP_ARROW='Right'
+LEFT_ARROW='Left'
+DOWN_ARROW='Down'
+RIGHT_ARROW='Right'
 SPACEBAR='space'
 
-UP='0'
-LEFT='1'
-DOWN='2'
-RIGHT='3'
+UP=0
+LEFT=1
+DOWN=2
+RIGHT=3
 direction=UP
-def up():
-    direction=UP
-    UP_ARROW
-    print('up arrow')
 
+
+
+turtle.onkeypress(up,UP_ARROW)
+turtle.onkeypress(down,DOWN_ARROW)
+turtle.onkeypress(left,LEFT_ARROW)
+turtle.onkeypress(right,RIGHT_ARROW)
+turtle.onkeypress(turtle.stamp,SPACEBAR)
+turtle.listen()
+turtle.mainloop()
